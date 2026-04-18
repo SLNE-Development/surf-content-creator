ae128489e4be56db8b8dfe71c3994fc44931ce41
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-04-18 23:15:30 +0200

    feat: update .gitignore and clean up ContentCreatorConfig.kt

--- surf-content-creator-core/src/main/kotlin/dev/slne/surf/content/creator/core/config/ContentCreatorConfig.kt
+++ surf-content-creator-core/src/main/kotlin/dev/slne/surf/content/creator/core/config/ContentCreatorConfig.kt
@@ -4,7 +4,6 @@ import dev.slne.surf.api.core.config.createSpongeYmlConfig
 import dev.slne.surf.api.core.config.surfConfigApi
 import dev.slne.surf.content.creator.core.coreApi
 import org.spongepowered.configurate.objectmapping.ConfigSerializable
-import kotlin.time.Duration.Companion.minutes
 
 @ConfigSerializable
 data class ContentCreatorConfig(
@@ -14,8 +13,7 @@ data class ContentCreatorConfig(
     @ConfigSerializable
     data class Twitch(
         val clientId: String = "",
-        val clientSecret: String = "",
-        val refreshIntervalSeconds: Long = 5.minutes.inWholeSeconds,
+        val clientSecret: String = ""
     )
 }
 
