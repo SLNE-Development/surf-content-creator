09a1ef824a3cb8ebc01c811162e79b80c8d5c4b8
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-04-18 23:07:36 +0200

    feat: rename velocity module to paper and update build configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,4 +6,4 @@ rootProject.name = "surf-content-creator"
 
 include("surf-content-creator-api")
 include("surf-content-creator-core")
-include("surf-content-creator-velocity")
+include("surf-content-creator-paper")
